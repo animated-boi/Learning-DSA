688f7a5974948285216d51ce2933246651d65c45
animated-boi <145880918+animated-boi@users.noreply.github.com>
2025-05-07 11:32:58 -0700

    LC 66 Updated Approach 2

--- LeetCode/LeetCode66.py
+++ LeetCode/LeetCode66.py
@@ -9,9 +9,32 @@
 #Approach One
 from typing import List
 
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         word = "".join(str(num) for num in digits)
+#         word = int(word) + 1
+
+#         return(list(int(i) for i in list(str(word))))
+    
+
+
+# #Approach2 - UNFINISHED
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        word = "".join(str(num) for num in digits)
-        word = int(word) + 1
+        carry = 1
+        
+        for i in range(len(digits)-1, -1, -1):
+            if carry:
+                if digits[i] != 9:
+                    digits[i] += 1
+                    carry = 0
+                else:
+                    digits[i] = 0
+        
+        if carry:        
+            digits = [1] + digits
 
-        return(list(int(i) for i in list(str(word))))
+        return digits
+
+sol = Solution()
+print(sol.plusOne([ 9, 9, 9]))
