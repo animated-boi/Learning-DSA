afcdc6eb4a1c8dab73705dcdc82461ea5161539e
animated-boi <145880918+animated-boi@users.noreply.github.com>
2025-05-11 10:20:30 -0700

    Updated LC 88

--- LeetCode/LeetCode88.py
+++ LeetCode/LeetCode88.py
@@ -9,32 +9,82 @@ The final sorted array should not be returned by the function, but instead be st
 
 '''
 from typing import List
+#Approach 1 - Extra Array    #Time = O(m+n) #Space = O(m+n)
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+#         """
+#         Do not return anything, modify nums1 in-place instead.
+#         """
+#         nums = [0]* (m+n)
+#         i, j, k = 0, 0, 0
+#         while i < m and j < n:
+#             if nums1[i] <= nums2[j]:
+#                 nums[k] = nums1[i]
+#                 i += 1
+#             else:
+#                 nums[k] = nums2[j]
+#                 j += 1
+#             k += 1
+        
+#         while i < m :
+#             nums[k] = nums1[i]
+#             i, k = i + 1, k + 1
+        
+#         while j < n :
+#                 nums[k] = nums2[j]
+#                 j, k = j + 1, k + 1
+               
+#         return nums
 
+
+#Approach 2 - In-place   #Time = O(m+n) #Space = O(1)
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+#         """
+#         Do not return anything, modify nums1 in-place instead.
+#         # """
+        # i = m - 1
+        # j = n - 1
+        # k = m + n -1
+        
+        # while i >= 0 and j >= 0:
+        #     if nums1[i] <= nums2[j]:
+        #         nums1[k] = nums2[j]
+        #         j -= 1
+        #     else:
+        #         nums1[k] = nums1[i]
+        #         i -= 1
+        #     k -= 1
+        
+        # while j >= 0 :
+        #         nums1[k] = nums2[j]
+        #         j, k = j - 1, k - 1
+        # while i >= 0 :
+        #         nums1[k] = nums1[i]
+        #         i, k = i - 1, k - 1
+
+        # return nums1
+
+#OPTIMISED APPROACH 2 - 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        """
-        nums = [0]* (m+n)
-        i, j, k = 0, 0, 0
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                nums[k] = nums1[i]
-                i += 1
+        # """
+        i = m - 1
+        j = n - 1
+        k = m + n -1
+
+        while j>= 0:
+            if i >= 0 and nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i = i - 1
             else:
-                nums[k] = nums2[j]
-                j += 1
-            k += 1
-        
-        while i < m :
-            nums[k] = nums1[i]
-            i, k = i + 1, k + 1
-        
-        while j < n :
-                nums[k] = nums2[j]
-                j, k = j + 1, k + 1
-               
-        return nums
+                nums1[k] = nums2[j]
+                j = j - 1 
+            k = k - 1
+            
+        return nums1
 
 sol = Solution()
 print(sol.merge([1,2,3,0,0,0], 3, [2,5,6], 3))
